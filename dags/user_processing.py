8aa6e77617b526019eae638b5597e9b97bdb5e14
cloud_actuary <pavel.korol.jobs@gmail.com>
2026-05-20 23:03:46 +0300

    create sensor task

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,9 +1,12 @@
-from airflow.sdk import dag
+from airflow.sdk import dag, task
+from airflow.sdk.bases.sensor import PokeReturnValue
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
 @dag
-def user_processing():
+def dag_user_processing():
+
+    # Execution task
     create_table = SQLExecuteQueryOperator(
         task_id = "create_table",
         conn_id = "pg_conn",
@@ -18,4 +21,17 @@ CREATE TABLE IF NOT EXISTS users (
         """
     )
 
-user_processing()
+    # Sensor Task
+    @task.sensor(poke_interval = 30, timeout = 600)
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        resp = requests.get("https://randomuser.me/api")
+        if resp.status_code == 200:
+            return PokeReturnValue(is_done = True, xcom_value = resp.json())
+        else:
+            return PokeReturnValue(is_done = False, xcom_value = None)
+
+    is_api_available()
+    
+dag_user_processing()
+
