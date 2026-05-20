233724e72fdb76230d0756d6eb306db6be95168f
cloud_actuary <pavel.korol.jobs@gmail.com>
2026-05-20 22:29:40 +0300

    Create task for create new user table in postgresql data server

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,7 +1,21 @@
 from airflow.sdk import dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
 
 @dag
 def user_processing():
-    pass
+    create_table = SQLExecuteQueryOperator(
+        task_id = "create_table",
+        conn_id = "pg_conn",
+        sql = """
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+        """
+    )
 
 user_processing()
