e858043555c7696056bdeffcc614afa319325581
cloud_actuary <pavel.korol.jobs@gmail.com>
2026-05-21 19:33:36 +0300

    Adding second asset, that connected to `user` asset

--- dags/user.py
+++ dags/user.py
@@ -1,5 +1,5 @@
 
-from airflow.sdk import asset
+from airflow.sdk import asset, Asset, Context
 
 @asset(
     schedule = "@daily",
@@ -13,3 +13,35 @@ def user(self) -> dict[str]:
     if req.status_code == 200:
         return req.json()
     return None
+
+
+# second asset, take location from user
+# this will run after the `user` asset materialized
+@asset(
+    schedule = user
+)
+def user_location(user: Asset, context: Context) -> dict[str]:
+
+    # if not user or "results" not in user:
+    #     return {}
+    
+    user_data = context['ti'].xcom_pull(
+        dag_id = user.name,
+        task_ids = user.name,
+        # latest user asset
+        include_prior_dates = True
+    )
+    print("-----")
+    print(user_data)
+    print("-----")
+    print(type(user_data))
+    print(len(user_data))
+    print("-----")
+    print(user_data[0].keys())
+
+    if not user_data or "results" not in user_data[0]:
+        print("ERRROR EXIT.....")
+        return {}
+
+    return user_data[0]["results"][0]["location"]
+
