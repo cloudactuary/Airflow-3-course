bcb4d41669262d26eb2babe095c58ea81f600502
cloud_actuary <pavel.korol.jobs@gmail.com>
2026-05-24 11:26:57 +0300

    branching in dag

---
+++ dags/bracnhing.py
@@ -0,0 +1,28 @@
+from airflow.sdk import dag, task
+
+
+@dag
+def branching_dag():
+
+    @task
+    def a():
+        return 1
+
+    @task.branch
+    def b(val: int):
+        return ['eq_1'] if val == 1 else ['neq_1']
+
+    @task
+    def eq_1(val: int):
+        print(f"equal 1: {val}")
+    
+    @task
+    def neq_1(val: int):
+        print(f"not equal 1: {val}")
+
+
+    val = a()
+    b(val) >> [eq_1(), neq_1()]
+
+
+branching_dag()
